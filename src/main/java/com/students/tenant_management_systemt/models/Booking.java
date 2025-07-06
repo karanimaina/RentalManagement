537dac4b038ed5750476ff97136b4fd194808587
karanimaina <karani.maina2010@gmail.com>
2025-07-06 20:39:09 +0300

    create booking  model

--- src/main/java/com/students/tenant_management_systemt/models/Booking.java
+++ src/main/java/com/students/tenant_management_systemt/models/Booking.java
@@ -1,21 +1,25 @@
 package com.students.tenant_management_systemt.models;
 
+import com.students.tenant_management_systemt.dto.Status;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Random;
+
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 @NoArgsConstructor
 @Entity(name = "TB_BOOKING")
 public class Booking extends BaseEntity{
     private Long houseId;
-    private String houseName;
+    private String apartmentName;
     private String tenantName;
     private String tenantEmail;
     private String tenantPhone;
@@ -23,7 +27,11 @@ public class Booking extends BaseEntity{
     private LocalDate startDate;
     private LocalDate endDate;
     private double totalAmount;
+    @Column(unique = true)
     private String bookingReference;
     private LocalDateTime bookingDateTime;
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @Column(unique = true)
+    private String houseCode;
 }
