a3b301e1723f876feb805bce9d26808939e08433
karanimaina <karani.maina2010@gmail.com>
2025-07-06 20:42:23 +0300

    create  TenantException model

---
+++ src/main/java/com/students/tenant_management_systemt/exception/TenantException.java
@@ -0,0 +1,7 @@
+package com.students.tenant_management_systemt.exception;
+
+public class TenantException extends RuntimeException{
+    public TenantException(String s) {
+        super(s);
+    }
+}
