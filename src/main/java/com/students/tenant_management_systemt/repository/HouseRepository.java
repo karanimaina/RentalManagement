f49416d57c123561b33cc13659c6069f975a369d
karanimaina <karani.maina2010@gmail.com>
2025-07-06 20:41:00 +0300

    create HouseRepository.java

--- src/main/java/com/students/tenant_management_systemt/repository/HouseRepository.java
+++ src/main/java/com/students/tenant_management_systemt/repository/HouseRepository.java
@@ -1,6 +1,13 @@
 package com.students.tenant_management_systemt.repository;
 
-public interface HouseRepository {
+import com.students.tenant_management_systemt.models.House;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
+public interface HouseRepository extends JpaRepository<House, Long> {
+
+    List<House>findByAvailableTrue();
+    List<House>findByLocationIgnoreCase(String location);
+    List<House>findHouseByPricePerDayBetween(Double min, Double max);
 }
